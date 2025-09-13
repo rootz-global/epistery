ecdbef422ad5f0a60dd4ec632c1ec3f84c6f37d2
michael <michael@sprague.com>
2025-09-13 17:54:52 -0400

    certify

--- cli/certify.mjs
+++ cli/certify.mjs
@@ -2,9 +2,6 @@
  * Domain Certification is built on certbot. It uses the Acme client to generate
  * ssl certificates through Let's Encrypt.
  */
-import express from 'express';
-import acme from 'acme-client';
-import tls from 'tls';
 import { SSLController } from '../dist/controllers/ssl/SSLController.js';
 
 export async function certify(context, domain) {
